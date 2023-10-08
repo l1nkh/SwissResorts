c963ed35f4f27d751fdf160f7f4456dee0f7f208
diogo-h <diogo.henriques@ontopstudios.com>
2023-10-08 17:04:27 +0100

    CRUD functionality completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Resort = require('./models/resort');
 
 mongoose.connect('mongodb://localhost:27017/swiss-resorts');
@@ -17,6 +18,9 @@ db.once("open", () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
@@ -26,10 +30,39 @@ app.get('/resorts', async (req, res) => {
     res.render('resorts/index', {resorts})
 })
 
+app.get('/resorts/new', (req, res) => {
+    res.render('resorts/new');
+})
+
+app.post('/resorts', async (req, res) => {
+    const resort = new Resort(req.body.resort);
+    await resort.save();
+    res.redirect(`/resorts/${resort._id}`);
+})
+
+
 app.get('/resorts/:id', async (req, res) => {
     const resort = await Resort.findById(req.params.id)
     res.render('resorts/show', { resort })
 })
+
+app.get('/resorts/:id/edit', async (req, res) => {
+    const resort = await Resort.findById(req.params.id)
+    res.render('resorts/edit', { resort })
+})
+
+app.put('/resorts/:id', async (req, res) => {
+    const { id } = req.params;
+    const resort = await Resort.findByIdAndUpdate(id, { ...req.body.resort });
+    res.redirect(`/resorts/${resort._id}`)
+});
+
+app.delete('/resorts/:id', async (req, res) => {
+    const { id } = req.params;
+    await Resort.findByIdAndDelete(id);
+    res.redirect('/resorts');
+})
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
